4f07f7987c795175531fc986159579260f292cc5
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2022-07-21 20:09:51 +0200

    crossing fingers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,6 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = fetch('data/photographers.json', { method: 'GET', })
+        const photographers = fetch('./data/photographers.json', { method: 'GET', })
         .then(response => response.json())
         .then(data => data.photographers)
 
